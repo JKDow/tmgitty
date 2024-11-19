9f6f0c3ee60ce9d86cb6ff22fbc355589840da14
Josh Dowling <jk.dowling22@gmail.com>
2024-11-20 00:32:22 +1100

    Added source to cargo ENV to enable cargo commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+source "$HOME/.cargo/env"
 cargo build --release --manifest-path "$SCRIPT_DIR/../Cargo.toml"
 
 mv "$SCRIPT_DIR/../target/release/tmgitty" "$SCRIPT_DIR/../bin"
